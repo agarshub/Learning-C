563260b2ee7fed720fab8023e776b00047084bc8
agarshub <shubhamagarwal0111@gmail.com>
2025-09-02 22:39:57 +0530

    code

--- p3.c
+++ p3.c
@@ -20,4 +20,3 @@ int main()
 
 // %d is used as a placeholder for integer values in the format string of printf and scanf functions.
 // & is the address operator. It is used to get the address of a variable. In scanf, it is used to store the input value at the address of the variable.
-
