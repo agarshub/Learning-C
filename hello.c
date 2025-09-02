2c4241829af39100ebf972579b6aed57c88683d0
agarshub <shubhamagarwal0111@gmail.com>
2025-09-02 18:37:12 +0530

    Hello

--- hello.c
+++ hello.c
@@ -4,9 +4,9 @@ The coder is free towrite anything here and this is not part of the code.
 This is just for the user to understand what the code is about.
 */
 
-#include<stdio.h>
+#include<stdio.h>  // This is a preprocessor directive. It contains the standard library files.
 
 int main()
 {
-    printf("Hello World!");
+    printf("Hello World!"); // printf is a function that prints the output on the screen.
 }
