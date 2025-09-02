27168fc943257a3258b1ba036bc10432eb764cb6
agarshub <shubhamagarwal0111@gmail.com>
2025-09-03 01:54:05 +0530

    code

--- p19.c
+++ p19.c
@@ -1,13 +1,30 @@
 /* Write a program to psolve res = b^2-4*a*c. */
 
 #include<stdio.h>
-#define res(a,b,c) (b*b)-(4*a*c)    // note that here we are using parentheses to avoid any operator precedence issues. and it is advisable to use parentheses in such cases.
+#define res(a,b,c) (b*b)-(4*a*c);    // note that here we are using parentheses to avoid any operator precedence issues. and it is advisable to use parentheses in such cases.
 int main()
 {
     int a,b,c;
     printf("Enter the values of a, b and c: ");
     scanf("%d%d%d",&a,&b,&c);
-    int result = res(a,b,c);
+    int result = res(a,b,c)
     printf("The result is: %d",result);
     return 0;
 }
+
+/* 
+To understand the concept of define better.. consider the following example:
+a=3,b=10,c=4
+Now, here in this code first the when the code takes three inputs a,b and c.. it stores them in the respective memory locations.
+Then when it comes to the line int result = res(a,b,c) , the preprocessor replaces res(a,b,c) with (b*b)-(4*a*c) , so the line becomes:
+so line 11 becomes:
+int result = (b*b)-(4*a*c);
+
+so then the ; which is must should not be written in the line 11 since it is already written in the #define directive.
+int result = (10*10)-(4*3*4);
+int result = 100 - 48;
+int result = 52;
+here 52 is stored in the memory location of result.
+printf("The result is: %d",result);
+The result is: 52
+*/
