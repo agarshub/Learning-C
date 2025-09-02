72360ddb6c177913b8761938f2b048ce61cce390
agarshub <shubhamagarwal0111@gmail.com>
2025-09-02 18:33:05 +0530

    Hello

---
+++ hello.c
@@ -0,0 +1,12 @@
+/*
+This is known as comment section. Where the user can write whatever he wants to without any restrictions.
+The coder is free towrite anything here and this is not part of the code.
+This is just for the user to understand what the code is about.
+*/
+
+#include<stdio.h>
+
+int main()
+{
+    printf("Hello World!");
+}
