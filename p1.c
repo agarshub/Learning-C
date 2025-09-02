39fb6500e691dbde8ff5a3ec8406bff294d09483
agarshub <shubhamagarwal0111@gmail.com>
2025-09-02 18:41:13 +0530

    p1

---
+++ p1.c
@@ -0,0 +1,13 @@
+/* Write a C program to print a message. */
+
+#include <stdio.h>
+int main()
+{
+    printf("Hello,\n");
+    printf("This is a simple C program!\n");
+    return 0;
+}
+
+// This program prints a message to the console.
+// The \n is used to insert a new line after the message.
+// A semi-colon (;) is used to terminate a statement in C.
