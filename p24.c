6f8673b6763c9d6ba404cf6bec11a2b7cc2609d0
agarshub <shubhamagarwal0111@gmail.com>
2025-09-04 23:44:09 +0530

    code

---
+++ p24.c
@@ -0,0 +1,23 @@
+/* Write a c prgram to give an example for size of operator. */
+
+#include<stdio.h>
+int main()
+{
+    short int a;
+    int y;
+    long int b;
+    long long int c;
+    float d;
+    double e;
+    long double f;
+    char g;
+    printf("The size of short int is: %zu bytes\n", sizeof(a));
+    printf("The size of int is: %zu bytes\n", sizeof(y));
+    printf("The size of long int is: %zu bytes\n", sizeof(b));
+    printf("The size of long long int is: %zu bytes\n", sizeof(c));
+    printf("The size of float is: %zu bytes\n", sizeof(d));
+    printf("The size of double is: %zu bytes\n", sizeof(e));
+    printf("The size of long double is: %zu bytes\n", sizeof(f));
+    printf("The size of char is: %zu bytes\n", sizeof(g));
+    return 0;
+}
