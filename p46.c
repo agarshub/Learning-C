0a8b7f41df7ecdb6bb9dd27177b3bb28868bb38c
agarshub <shubhamagarwal0111@gmail.com>
2025-09-05 01:05:19 +0530

    code

--- p46.c
+++ p46.c
@@ -9,3 +9,9 @@ int main()
     printf("The ASCII value of %c is: %d\n", x, x);
     return 0;
 }
+
+/*
+When a character is used in a context where an integer is expected, it is automatically converted to its corresponding ASCII value.
+    For example, if the character 'A' is used in an arithmetic operation, it will be treated as the integer 65 (the ASCII value of 'A').
+    This is why we can directly print the character variable 'x' using the %d format specifier to get its ASCII value.
+*/
