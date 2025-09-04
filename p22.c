efcc17427b0c98327bb0b5b69f015cca65b19b36
agarshub <shubhamagarwal0111@gmail.com>
2025-09-04 23:37:07 +0530

    code

--- p22.c
+++ p22.c
@@ -5,10 +5,14 @@ int main()
 {
     int a = 10; // Here '=' is an assignment operator that assigns the value 10 to the variable 'a'.
     // This is known as Declarativee assignment.
+    printf("The value of a is: %d\n", a);
 
     a = 30; // Here the value of 'a' is updated to 30 using the assignment operator '='.
     // This is known as Re-assignement/Normal assignment to an already initialized variable.
-    
-    printf("The value of a is: %d", a); // This will print the value of 'a' which is 30.
+    printf("The value of a is: %d\n", a);
+
+    a+= 20; // Here the value of 'a' is updated to 50 using the assignment operator '+='.
+    // This is known as Compound assignment/Arithmetic assignment.
+    printf("The value of a is: %d\n", a);
     return 0;
 }
