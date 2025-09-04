2f56b1f9700b119dc9705b437e7569f79aac9368
agarshub <shubhamagarwal0111@gmail.com>
2025-09-05 00:51:23 +0530

    code

---
+++ p43.c
@@ -0,0 +1,11 @@
+/* Write a program to read the year as input & check whether it is leap year/not. */
+
+#include<stdio.h>
+int main()
+{
+    int year;
+    printf("Enter the year: ");
+    scanf("%d", &year);
+    (year % 4 == 0 && year % 100 != 0) ? printf("%d is a leap year.\n", year) : (year % 400 == 0) ? printf("%d is a leap year.\n", year) : printf("%d is not a leap year.\n", year);
+    return 0;
+}
