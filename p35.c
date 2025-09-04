0cf0fb63ae25b597c6a68e562420ff84d49b9506
agarshub <shubhamagarwal0111@gmail.com>
2025-09-05 00:17:41 +0530

    code

---
+++ p35.c
@@ -0,0 +1,16 @@
+/* without using output variable do shift left and shift right. */
+
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+    printf("The double of the value n is %d \n", n << 1); // n * 2^1 = n * 2
+    printf("The half of the value n is %d \n", n >> 1); // n / 2^1 = n / 2
+    printf("The value of n multiplied by 4 is %d \n", n << 2); // 2^2 = 4
+    printf("The value of n divided by 4 is %d\n ", n >> 2); // 2^2 = 4
+    printf("The value of n multiplied by 8 is %d \n", n << 3); //2^3 = 8
+    printf("The value of n divided by 8 is %d \n", n >> 3); //2^3 = 8
+    return 0;
+}
