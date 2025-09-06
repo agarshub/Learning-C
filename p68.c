c57c6594467ca75297e419279e958cf7a522f05f
agarshub <shubhamagarwal0111@gmail.com>
2025-09-07 00:41:41 +0530

    code

--- p68.c
+++ p68.c
@@ -1,1 +1,18 @@
-/* */
+/* Write a program to read the no & check whether it is five digit number or not. */
+
+#include<stdio.h>
+int main()
+{
+    long int n;
+    printf("Enter the int value: ");
+    scanf("%d",&n);
+
+    if((n>=10000) && (n<=99999))
+    {
+        printf("Five Digit No");
+    }
+    else
+    {
+        printf("Not five digit No");
+    }
+}
