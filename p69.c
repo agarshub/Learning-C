01d7b6048bd374e45b2947ad8c8226054d9424f7
agarshub <shubhamagarwal0111@gmail.com>
2025-09-07 00:41:46 +0530

    code

--- p69.c
+++ p69.c
@@ -1,1 +1,19 @@
-/* */
+/* Write a program to read the chapter whether its upper or not (use character constants). */
+
+#include<stdio.h>
+int main()
+{
+    char x;
+    printf("Enter the character: ");
+    x=getchar();
+
+    if((x>='A')&&(x<='Z'))
+    {
+        printf("Upper\n");
+    }
+    else
+    {
+        printf("Not Upper\n");
+    }
+    return 0;
+}
