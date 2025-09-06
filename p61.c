89aeceda1077f7e90d34493d709b38a413085686
agarshub <shubhamagarwal0111@gmail.com>
2025-09-06 23:56:15 +0530

    code

---
+++ p61.c
@@ -0,0 +1,24 @@
+/* Write a program to read a value and find whether it is positive. */
+
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("Enter a number: ");
+    scanf("%d",&n);
+    if(n>=0)
+    {
+        printf("Positive Value");
+    }
+}
+
+/* This is simple if statement 
+
+Syntax:
+if(condition)
+{
+}
+
+note that when it is single stament inside {} we donot require them but it is always advisable to keep them to avoid confusion.
+
+*/
