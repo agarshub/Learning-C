6c64cb0d6205433d919837e0aa4eaedb3872c8c4
agarshub <shubhamagarwal0111@gmail.com>
2025-09-07 00:41:51 +0530

    code

--- p70.c
+++ p70.c
@@ -1,1 +1,19 @@
-/**/
+/* Write a program to read the character whether its upper or not using ASCII code. */
+
+#include<stdio.h>
+int main()
+{
+    char x;
+    printf("Enter the char: ");
+    x=getchar();
+
+    if((x>=65)&&(x<=90))
+    {
+        printf("Upper\n");
+    }
+    else
+    {
+        printf("Not Upper\n");
+    }
+    return 0;
+}
