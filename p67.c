7ef39f66afefefb2e8ad3800a57ac899a5c3268d
agarshub <shubhamagarwal0111@gmail.com>
2025-09-07 00:41:37 +0530

    code

--- p67.c
+++ p67.c
@@ -1,1 +1,19 @@
-/* */
+/* Write a program to find absolute value of the given integer. */
+
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("Enter the n value : ");
+    scanf("%d",&n);
+    
+    if(n<=0)
+    {
+        printf("Absolute Value is %d\n",n*(-1));
+    }
+    else
+    {
+        printf("Absolute value is %d\n",n);
+    }
+    return 0;
+}
