c052281a643b2f263131a8aa729d3da8dc06e47c
agarshub <shubhamagarwal0111@gmail.com>
2025-09-07 13:40:23 +0530

    code

--- p79.c
+++ p79.c
@@ -11,6 +11,29 @@ age     category
 #include<stdio.h>
 int main()
 {
-    
+    int age;
+    printf("Enter the age of the person: ");
+    scanf("%d",&age);
+
+    if(age>=1  && age<=12)
+    {
+        printf("Child\n");
+    }
+    else if(age>=13 && age<=19)
+    {
+        printf("Teenage\n");
+    }
+    else if(age>=20 && age<=30)
+    {
+        printf("Young Age\n");
+    }
+    else if(age>=31 && age<=45)
+    {
+        printf("Middle Age\n");
+    }
+    else if(age >45)
+    {
+        printf("Old Age");
+    }
     return 0;
 }
