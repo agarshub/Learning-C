9e257ca21f9c4208165f524d76f5693160b548db
agarshub <shubhamagarwal0111@gmail.com>
2025-09-07 13:36:39 +0530

    code

---
+++ p79.c
@@ -0,0 +1,16 @@
+/* Write a program to read the age of a person and check whether the persoin belongs to which one of the following category. */
+/*
+age     category
+1-12    child
+13-19   Teenage
+20-30   Young Age
+31-45   Middle Age
+>45     Old Age
+*/
+
+#include<stdio.h>
+int main()
+{
+    
+    return 0;
+}
