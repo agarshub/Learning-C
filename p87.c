99e6bb9ef78e1d61326d42e4cdcbb1f28192c18a
agarshub <shubhamagarwal0111@gmail.com>
2025-09-30 00:01:54 +0530

    code

--- p87.c
+++ p87.c
@@ -1,7 +1,43 @@
+/* Write a program to read the first character of the day and print which day it is*/
+
 #include<stdio.h>
 int main()
 {
-	int n;
+	char ch;
+	printf("m.Monday\n");
+	printf("t.Tuesday\n");
+	printf("w.Wednesday\n");
+	printf("h.Thursday\n");
+	printf("f.Friday\n");
+	printf("s.Saturday\n");
+	printf("u.Sunday\n");
+	
+	printf("Enter the choice: ");
+	ch=getchar();
+	switch(ch)
+	{
+		case 'm': printf("Monday\n");
+		break;
 
+		case 't':printf("Tuesday\n");
+		break;
+
+		case 'w': printf("Wednesday\n");
+		break;
+
+		case 'h': printf("Thursday\n");
+		break;
+
+		case 'f': printf("Friday\n");
+		break;
+
+		case 's': printf("Saturday\n");
+		break;
+
+		case 'u': printf("Sunday\n");
+		break;
+
+		default: printf("Invalid Input\n");
+	}
 	return 0;
 }
