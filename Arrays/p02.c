64397ef2145eaa49eeb038da33fd4a11f2b836ba
agarshub <shubhamagarwal0111@gmail.com>
2025-10-03 23:55:45 +0530

    code

---
+++ Arrays/p02.c
@@ -0,0 +1,20 @@
+/* Write a program to read 5 values in the array and print the array*/
+
+#include<stdio.h>
+int main()
+{
+    int a[5]; // Declare an array of size 5
+
+    for(int i = 0; i < 5; i++)  // loop of 5 iterations from 0 to 4
+    {
+        printf("Enter value for a[%d]: ", i);
+        scanf("%d", &a[i]);
+    }
+
+    for(int i = 0; i < 5; i++)
+    {
+        printf("a[%d] = %d\n", i, a[i]);
+    }
+
+    return 0;
+}
