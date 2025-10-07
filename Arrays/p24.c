c231f9118698c2f37c9f65adad16afdf6ce943db
agarshub <shubhamagarwal0111@gmail.com>
2025-10-07 23:50:43 +0530

    code

--- Arrays/p24.c
+++ Arrays/p24.c
@@ -1,6 +1,7 @@
 /* Write a program to find length of the string.. */
 
 #include<stdio.h>
+#include<string.h>
 int main()
 {
     char x[40];
@@ -13,6 +14,11 @@ int main()
     
     l=strlen(x);
     printf("The no of char in a string is %d\n",l);
-    
+
     return 0;
 }
+
+/* 
+strlen is a built in function used like printf and scanf..
+this function is in a library string.h
+*/
