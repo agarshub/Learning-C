4103a201a713c8a06533433db15eb1c2a8de9dcc
agarshub <shubhamagarwal0111@gmail.com>
2025-10-08 23:42:42 +0530

    code

---
+++ Strings/p03.c
@@ -0,0 +1,18 @@
+/* Write a program to convert the string into uppercase. */
+
+#include<stdio.h>
+
+int main()
+{
+    char x[40];
+    printf("Enter the string: ");
+    gets(x);
+
+    printf("The given string is: ");
+    puts(x);
+
+    strupr(x);
+    printf("The string in upper case");
+    
+    return 0;
+}
