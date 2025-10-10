e83e0a5de5d90b511bc4649c9f2c3626c82e34de
agarshub <shubhamagarwal0111@gmail.com>
2025-10-10 23:49:36 +0530

    code

---
+++ Strings/p13.c
@@ -0,0 +1,29 @@
+/* Give an example for concatenation.. */
+
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+    char x[40],y[40];
+    printf("Enter two strings: ");
+    gets(x);
+    gets(y);
+
+    printf("The given strings %s %s \n",x,y);
+    strcat(x,y);
+
+    printf("The final string is %s \n",x);
+
+    return 0;
+}
+
+/*
+concatenation is a part where 
+x = Hello
+y = World
+
+strcat(x,y) concatenates string y after x..
+output:
+x= HelloWorld
+*/
